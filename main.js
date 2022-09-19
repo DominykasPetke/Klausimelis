bd836340414d9d729d4a5ddae133914f8442abcc
Dominykas <dominykas.petkevicius@gmail.com>
2022-09-19 11:40:03 +0300

    Made sure JSHint is happy

--- main.js
+++ main.js
@@ -1,49 +1,49 @@
-'use strict'
+/*jshint node: true */
+/*jshint esversion: 6 */
+'use strict';
 
 // express.js init
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 // db init
-const mysql = require('mysql')
+const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'dominykas',
     password: 'password',
     database: 'test'
-})
+});
 
-connection.connect()
+connection.connect();
 
 connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
-    if (err) throw err
+    if (err) throw err;
 
-    console.log('The solution is: ', rows[0].solution)
-})
+    console.log('The solution is: ', rows[0].solution);
+});
 
-connection.end()
+connection.end();
 
+
+app.get('/:text', (req, res) => {
+    res.send('Hello ' + req.params.text + '!' );
+});
 
 app.get('/', (req, res) => {
-
-    res.send('Hello World!')
-})
-
-app.get('/wtf.test', (req, res) => {
-    res.status(202).send('Hello WTFFFFFzz2!')
-})
-
+    res.status(202).send('No params');
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Klausimėlis listening on port ${port}`);
+});
 
 app.use((req, res, next) => {
-    res.status(404).send("Sorry can't find that!")
-})
+    res.status(404).send("Sorry can't find that!");
+});
 
 app.use((err, req, res, next) => {
-    console.error(err.stack)
-    res.status(500).send('Something broke!')
-})
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
