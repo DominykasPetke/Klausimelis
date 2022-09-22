2928691df4ac33d5301290299d195a096828d0df
Dominykas <dominykas.petkevicius@gmail.com>
2022-09-22 17:22:44 +0300

    Base API for one resource

--- main.js
+++ main.js
@@ -26,18 +26,45 @@ connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
 
 connection.end();
 
+const api_header = "/api/v1";
+const not_implemented_error = { code: 501, err: "NOT_IMPLEMENTED", message: "Not implemented (yet)" };
 
-app.get('/:text', (req, res) => {
-    res.send('Hello ' + req.params.text + '!');
+app.get(api_header + '/topics', (req, res) => {
+    var ret = not_implemented_error;
+    ret.text = "GET REQUEST";
+    res.status(501).json(ret);
 });
 
-app.get('/', (req, res) => {
-    if (req.query.name != null) {
-        res.send('Hi ' + req.query.name + '!');
-    }
-    else {
-        res.status(202).send('No params');
-    }
+app.get(api_header + '/topics/:topicId', (req, res) => {
+    var ret = not_implemented_error;
+    ret.id = req.params.topicId;
+    res.status(501).json(ret);
+});
+
+app.post(api_header + '/topics', (req, res) => {
+    var ret = not_implemented_error;
+    ret.text = "POST REQUEST";
+    res.status(501).json(ret);
+});
+
+app.put(api_header + '/topics/:topicId', (req, res) => {
+    var ret = not_implemented_error;
+    ret.id = req.params.topicId;
+    ret.text = "PUT";
+    res.status(501).json(ret);
+});
+
+app.patch(api_header + '/topics', (req, res) => {
+    var ret = not_implemented_error;
+    ret.text = "PATCH";
+    res.status(501).json(ret);
+});
+
+app.delete(api_header + '/topics/:topicId', (req, res) => {
+    var ret = not_implemented_error;
+    ret.id = req.params.topicId;
+    ret.text = "DELETE";
+    res.status(501).json(ret);
 });
 
 app.listen(port, () => {
@@ -45,10 +72,10 @@ app.listen(port, () => {
 });
 
 app.use((req, res, next) => {
-    res.status(404).send("Sorry can't find that!");
+    res.status(404).json({ code: 404, err: "NOT_FOUND", message: "Page not found" });
 });
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).send('Something broke!');
+    res.status(500).json({ code: 500, err: "SERVER_ERROR", message: "Internal Server Error" });
 });
