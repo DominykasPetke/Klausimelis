2db6dfe3162d5b15ecf0ff53c0daaec2b46f6a1e
Dominykas <dominykas.petkevicius@gmail.com>
2022-09-29 12:41:50 +0300

    Forgor return YEO

--- main.js
+++ main.js
@@ -321,6 +321,7 @@ app.use((req, res, next) => {
 app.use(function (err, req, res, next) {
     if (err.type == "entity.parse.failed") {
         res.status(400).json({ code: 400, err: "BAD_REQUEST", message: "JSON parsing failed" });
+        return;
     }
 
     error500(err, req, res, next);
