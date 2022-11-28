f1d7e4873b301423668f2efd30285561fa27d8b1
Dominykas <dominykas.petkevicius@gmail.com>
2022-11-28 11:53:46 +0200

    Hamborgor design

--- frontend/src/components/MenuButton.vue
+++ frontend/src/components/MenuButton.vue
@@ -18,8 +18,22 @@ defineProps({
 </template>
 
 <style>
-.button {
-  height: 100%;
-  margin: 0 0.5rem;
+@media (max-width: 1024px) {
+  .button {
+    margin: 0;
+    width: 100%;
+    border-style: solid;
+    border-width: 1px;
+    border-color: rgba(102, 153, 0, 0.2);
+    background-color: rgba(255, 255, 255, 0.1);
+    text-align: center;
+  }
+}
+
+@media (min-width: 1024px) {
+  .button {
+    height: 100%;
+    margin: 0 0.5rem;
+  }
 }
 </style>
