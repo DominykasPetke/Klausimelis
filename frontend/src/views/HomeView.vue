4add1da533dbea476cd76cf380295cda02236ec5
Dominykas <dominykas.petkevicius@gmail.com>
2022-11-29 10:33:22 +0200

    Propah home page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,22 @@
-<script setup>
-import TheWelcome from "../components/TheWelcome.vue";
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="center">
+    <img
+      alt="Klausimėlis logo"
+      class="logo"
+      src="src/assets/question.svg"
+      width="512"
+      height="512"
+    />
+    <h1>Sveiki atvykę į Klausimėlį!</h1>
+  </div>
 </template>
+
+<style>
+.center {
+  text-align: center;
+  color: rgb(102, 153, 0);
+  padding: 1em;
+}
+</style>
