e7e5194e3cdbe200eeaed33d31360eceed477c8a
Dominykas <dominykas.petkevicius@gmail.com>
2022-11-29 13:12:50 +0200

    Modal gang

---
+++ frontend/src/modals/TopicsModal.vue
@@ -0,0 +1,60 @@
+<script setup>
+const emit = defineEmits(["close"]);
+
+function close() {
+  emit("close");
+}
+</script>
+
+<template>
+  <div class="modal-backdrop">
+    <div class="modal">
+      <p>A LOT MORE TEXT!</p>
+      <button type="button" class="btn-close" @click="close">x</button>
+    </div>
+  </div>
+</template>
+
+<style>
+.modal-backdrop {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal {
+  background: var(--color-background-soft);
+  box-shadow: 8px 8px 20px 1px black;
+  border-color: black;
+  overflow-x: auto;
+  display: flex;
+  flex-direction: column;
+  padding: 2em 1em;
+}
+
+.btn-close {
+  position: absolute;
+  top: 0;
+  right: 0;
+  border: none;
+  font-size: 20px;
+  padding: 10px;
+  cursor: pointer;
+  font-weight: bold;
+  color: #4aae9b;
+  background: transparent;
+}
+
+.btn-green {
+  color: white;
+  background: #4aae9b;
+  border: 1px solid #4aae9b;
+  border-radius: 2px;
+}
+</style>
--- frontend/src/views/ThemesView.vue
+++ frontend/src/views/ThemesView.vue
@@ -1,3 +1,23 @@
-<template>Themes</template>
+<script setup>
+import TopicsModal from "../modals/TopicsModal.vue";
+import { ref } from "vue";
+
+const isModalVisible = ref(false);
+
+function showModal() {
+  isModalVisible.value = true;
+}
+
+function closeModal() {
+  isModalVisible.value = false;
+}
+</script>
+
+<template>
+  Themes
+  <button type="button" class="btn" @click="showModal">Open Modal!</button>
+
+  <TopicsModal v-show="isModalVisible" @close="closeModal" />
+</template>
 
 <style></style>
