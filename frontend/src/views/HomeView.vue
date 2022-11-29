b2f79bfcf13f016b747172ef0241828141601c0e
Dominykas <dominykas.petkevicius@gmail.com>
2022-11-29 15:01:12 +0200

    Auto-resizable image

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,13 +2,7 @@
 
 <template>
   <div class="center">
-    <img
-      alt="Klausimėlis logo"
-      class="logo"
-      src="../assets/question.svg"
-      width="512"
-      height="512"
-    />
+    <img alt="Klausimėlis logo" class="homeLogo" src="../assets/question.svg" />
     <h1>Sveiki atvykę į Klausimėlį!</h1>
   </div>
 </template>
@@ -18,5 +12,10 @@
   text-align: center;
   color: rgb(102, 153, 0);
   padding: 1em;
+}
+
+.homeLogo {
+  max-width: min(80vw, 512px);
+  object-fit: contain;
 }
 </style>
