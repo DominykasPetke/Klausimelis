9fc9e90fe06b555cc679a6c9334d363e4855f25a
Dominykas <dominykas.petkevicius@gmail.com>
2022-11-30 17:51:38 +0200

    Added base register page

--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -1,3 +1,82 @@
-<template>Registracija</template>
+<script setup>
+import { ref, inject } from "vue";
 
-<style></style>
+const baseAPIURL = inject("baseAPIURL");
+
+const email = ref("");
+const password = ref("");
+const username = ref("");
+
+const isError = ref(false);
+const error = ref(null);
+
+const registeredUser = ref("");
+const isValid = ref(false);
+
+async function getLogin() {
+  return fetch(baseAPIURL + "/register", {
+    method: "POST",
+    body: JSON.stringify({
+      email: email.value,
+      password: password.value,
+      username: username.value,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => {
+      // a non-200 response code
+      if (!res.ok) {
+        // create error instance with HTTP status text
+        const error = new Error(res.statusText);
+        error.json = res.json();
+        throw error;
+      }
+
+      const ret = res.json();
+      return ret;
+    })
+    .then((json) => {
+      registeredUser.value = json.username;
+      isValid.value = true;
+    })
+    .catch((e) => {
+      isError.value = true;
+
+      if (e.message == "Unauthorized") {
+        error.value = "Neteisingas el. paštas arba slaptažodis";
+      } else {
+        error.value = e;
+      }
+    });
+}
+</script>
+
+<template>
+  <h1>Registracijos forma</h1>
+  <div class="input">
+    Slapyvardis:
+    <input type="text" v-model="username" />
+  </div>
+  <div class="input">
+    El. paštas:
+    <input type="email" v-model="email" />
+  </div>
+  <div class="input">
+    Slaptažodis:
+    <input type="password" v-model="password" />
+  </div>
+  <button @click="getLogin">Prisijungti</button>
+  <div v-show="isError">{{ error }}</div>
+  <div v-show="isValid">
+    Sėkmingai užregistruotas vartotojas {{ registeredUser }}.
+  </div>
+</template>
+
+<style>
+.input {
+  display: grid;
+  grid-template-columns: 100px 300px;
+}
+</style>
